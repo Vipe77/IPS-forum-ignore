6caaa1d4efdb253ead151cfad16706108dd7f3d3
rZr <opicron@gmail.com>
2013-08-12 19:36:16 +0200

    Multi user support

--- Robert Space Forum - Ignore User Script.js
+++ Robert Space Forum - Ignore User Script.js
@@ -5,7 +5,7 @@
 // @grant       none
 // ==/UserScript==
 
-var hideUser = "userName";
+var userNames = [ "firstUser", "secondUser" ];
 var completeHidePosts = false;
 var completeHideQuotes = false;
 
@@ -16,60 +16,64 @@ posts.each(function(index ) {
     
     // check Author in posts
     
-    var check = post.find("div.header div.Author div.AuthorInfo a.Username:contains('"+hideUser+"');");
-    if (check.length > 0)
+    for (var i in userNames)
     {
-        if (completeHidePosts)
-        {
-            post.hide();
-        }
-        else
-        {
-            var article = post.find("div.section div.article").hide();
-            article.before("<div class='article'><a class='ignoreUser'>Show hidden post from "+hideUser+"</a></div>");
-            
-            post.find("a.ignoreUser").click( function() {
-                var article = $(this).parent().next("div.article");
-                if (article.is(":visible"))
-                {
-                    $(this).text("Show hidden post from "+hideUser+"");                
-                    article.hide();
-                }
-                else
-                {
-                    $(this).text("Hide post from "+hideUser+"");
-                    article.show();
-                }
-                 
-            });            
-        }      
-    }
-    
-    // check Author in quotes
-    
-    var quotes = post.find("div.section div.Message blockquote.UserQuote");
-
-    quotes.each(function(index ) {    
-        var quote = $(this);
-        var check = quote.find("div.QuoteAuthor:contains('"+hideUser+"')");
-        
+        var hideUser = userNames[i];
+        var check = post.find("div.header div.Author div.AuthorInfo a.Username:contains('"+hideUser+"');");
         if (check.length > 0)
         {
-            if (!completeHideQuotes)
-            {                 
-                quote.before("<div><a class='ignoreLink'>Hidden quote from "+hideUser+"</a> <a class='QuoteFolding' href=''>» show previous quotes</a></div>");
-                var ignoreLink = $("a.ignoreLink:last");
-                ignoreLink.click(function() {
-                    $(this).next().click();                
-                }); 
-                ignoreLink.next().hide();
-                
-                quote.addClass("QuoteFolded");
-                
-                quote.after("<br>");               
+            if (completeHidePosts)
+            {
+                post.hide();
             }
-            
-            quote.hide();            
+            else
+            {
+                var article = post.find("div.section div.article").hide();
+                article.before("<div class='article'><a class='ignoreUser'>Show hidden post</a></div>");
+                
+                post.find("a.ignoreUser").click( function() {
+                    var article = $(this).parent().next("div.article");
+                    if (article.is(":visible"))
+                    {
+                        $(this).text("Show hidden post");                
+                        article.hide();
+                    }
+                    else
+                    {
+                        $(this).text("Hide post");
+                        article.show();
+                    }
+                     
+                });            
+            }      
         }
-    });
+        
+        // check Author in quotes
+        
+        var quotes = post.find("div.section div.Message blockquote.UserQuote");
+
+        quotes.each(function(index ) {    
+            var quote = $(this);
+            var check = quote.find("div.QuoteAuthor:contains('"+hideUser+"')");
+            
+            if (check.length > 0)
+            {
+                if (!completeHideQuotes)
+                {                 
+                    quote.before("<div><a class='ignoreLink'>Toggle visbility quote from "+hideUser+"</a> <a class='QuoteFolding' href=''>» show previous quotes</a></div>");
+                    var ignoreLink = $("a.ignoreLink:last");
+                    ignoreLink.click(function() {
+                        $(this).next().click();                
+                    }); 
+                    ignoreLink.next().hide();
+                    
+                    quote.addClass("QuoteFolded");
+                    
+                    quote.after("<br>");               
+                }
+                
+                quote.hide();            
+            }
+        });
+    }
 });
