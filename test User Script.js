70358e211367f27aab61bf6d56c5d2c28a2c2224
Vipe77 <Vipe77@users.noreply.github.com>
2014-10-12 14:27:11 +0400

    Create test User Script.js

---
+++ test User Script.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name Vipe's test cript
+// @namespace https://github.com/Vipe77/IPS-forum-ignore/new/master/test
+// @description Made for testing
+// @include http://yarportal.ru/*
+// @include http://www.yarportal.ru/*
+
